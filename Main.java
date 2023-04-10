fe1fd22248a3712db81729c9814a1fe5b5683d16
claraxzy <126642454+claraxzy@users.noreply.github.com>
2023-04-10 09:59:11 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        item[] listitem= new item[3];
-        listitem[0]= new item(1000,"potion","S",3);
-        listitem[1]= new item(5000,"Sword","A",1);
-        listitem[2]= new item(10000,"Armor","A",1);
-        item.seeAllitem();
-    }
-}
