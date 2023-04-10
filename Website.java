7ea7c4fce58d6ae4942b7bf8a3fa7a93b6df69d3
claraxzy <126642454+claraxzy@users.noreply.github.com>
2023-04-10 09:56:38 +0700

    Add files via upload

---
+++ Website.java
@@ -0,0 +1,18 @@
+public class Website extends Account{
+    private int index;
+    private String usernames;
+    private String password;
+
+    public Website() {
+    }
+    public void addAccount(String username, String password){
+        super(username,password);
+    }
+
+
+//    public void login(){
+//
+//    }
+//    public String encrypt(){
+//    }
+}
